93a97ec75b0041f481dbb14c156ee3892c75d92f
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-13 16:57:16 +0300

    fix(dram): refactor address calculations in drmLoad and drmStore functions for clarity

--- src/dram.c
+++ src/dram.c
@@ -2,14 +2,16 @@
 
 #pragma bss-name(push, "ZEROPAGE")
 static u32 buffer;
+unsigned long offset;
 #pragma bss-name(pop)
 
 u32* __fastcall__ drmLoad(struct DRAM* dram, unsigned long address) {
     if (address >= DRAM_BASE && address <= DRAM_BASE + DRAM_SIZE - 4) {
-        buffer.b[0] = dram->mem[address - DRAM_BASE];
-        buffer.b[1] = dram->mem[address - DRAM_BASE + 1];
-        buffer.b[2] = dram->mem[address - DRAM_BASE + 2];
-        buffer.b[3] = dram->mem[address - DRAM_BASE + 3];
+        offset = address - DRAM_BASE;
+        buffer.b[0] = dram->mem[offset];
+        buffer.b[1] = dram->mem[offset + 1];
+        buffer.b[2] = dram->mem[offset + 2];
+        buffer.b[3] = dram->mem[offset + 3];
         
         return &buffer;
     }
@@ -18,10 +20,11 @@ u32* __fastcall__ drmLoad(struct DRAM* dram, unsigned long address) {
 }
 
 void __fastcall__ drmStore(struct DRAM* dram, unsigned long address, u32* value) {
-    if (address < DRAM_SIZE - 3) {
-        dram->mem[address] = value->b[0];
-        dram->mem[address + 1] = value->b[1];
-        dram->mem[address + 2] = value->b[2];
-        dram->mem[address + 3] = value->b[3];
+    if (address >= DRAM_BASE && address <= DRAM_BASE + DRAM_SIZE - 4) {
+        offset = address - DRAM_BASE;
+        dram->mem[offset] = value->b[0];
+        dram->mem[offset + 1] = value->b[1];
+        dram->mem[offset + 2] = value->b[2];
+        dram->mem[offset + 3] = value->b[3];
     }
 }
