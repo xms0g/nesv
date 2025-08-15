8907cf86c726be7356a703a80ec63765bd6b1ad4
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-15 12:58:20 +0300

    refactor(nesio): enhance character positioning by adding vram_adr calls in macros

--- src/nesio.h
+++ src/nesio.h
@@ -5,29 +5,26 @@
 #include "neslib.h"
 #include "types.h"
 
-#define NEXT_CHAR(n) do { x += (n); } while(0)
+#define NEXT_CHAR(n) do { x += (n); vram_adr(NTADR_A(x, y)); } while(0)
 
 #define PUT(str) do { \
-    vram_adr(NTADR_A(x, y)); \
     print(str); \
     NEXT_CHAR(strlen(str) + 1); \
     } while(0)
 
 #define PUTR(r, v) do { \
-    vram_adr(NTADR_A(x, y)); \
     printReg(r, v); \
     if (v & 0x800) NEXT_CHAR(5); \
     else NEXT_CHAR(4); \
     } while(0)
 
 #define PUTI(v) do { \
-    vram_adr(NTADR_A(x, y)); \
     printImm(v); \
     if (v & 0x800) NEXT_CHAR(5); \
     else NEXT_CHAR(4); \
     } while(0)
 
-#define NEXT_LINE() do { x = 1; ++y; } while(0)
+#define NEXT_LINE() do { x = 1; ++y; vram_adr(NTADR_A(x, y)); } while(0)
 
 void __fastcall__ print(const unsigned char* str);
 
