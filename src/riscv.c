b0e758b8e0d862172dc11eb64ed37babc32799ff
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-17 22:56:58 +0300

    feat(riscv):  implemented lh/lw/lbu/lhu sh/sw

--- src/riscv.c
+++ src/riscv.c
@@ -2,75 +2,6 @@
 #include "neslib.h"
 #include "nesio.h"
 
-#define X0  R_ZERO
-#define X1  R_RA
-#define X2  R_SP
-#define X3  R_GP
-#define X4  R_TP
-#define X5  R_T0
-#define X6  R_T1
-#define X7  R_T2
-#define X8  R_S0
-#define X9  R_S1
-#define X10 R_A0
-#define X11 R_A1
-#define X12 R_A2
-#define X13 R_A3
-#define X14 R_A4
-#define X15 R_A5
-#define X16 R_A6
-#define X17 R_A7
-#define X18 R_S2
-#define X19 R_S3
-#define X20 R_S4
-#define X21 R_S5
-#define X22 R_S6
-#define X23 R_S7
-#define X24 R_S8
-#define X25 R_S9
-#define X26 R_S10
-#define X27 R_S11
-#define X28 R_T3
-#define X29 R_T4
-#define X30 R_T5
-#define X31 R_T6
-
-enum Registers {
-    R_ZERO = 0,
-    R_RA = 1,
-    R_SP = 2,
-    R_GP = 3,
-    R_TP = 4,
-    R_T0 = 5,
-    R_T1 = 6,
-    R_T2 = 7,
-    R_S0 = 8,
-    R_FP = 8,
-    R_S1 = 9,
-    R_A0 = 10,
-    R_A1 = 11,
-    R_A2 = 12,
-    R_A3 = 13,
-    R_A4 = 14,
-    R_A5 = 15,
-    R_A6 = 16,
-    R_A7 = 17,
-    R_S2 = 18,
-    R_S3 = 19,
-    R_S4 = 20,
-    R_S5 = 21,
-    R_S6 = 22,
-    R_S7 = 23,
-    R_S8 = 24,
-    R_S9 = 25,
-    R_S10 = 26,
-    R_S11 = 27,
-    R_T3 = 28,
-    R_T4 = 29,
-    R_T5 = 30,
-    R_T6 = 31
-};
-
 #pragma bss-name(push, "ZEROPAGE")
 static unsigned char x;
 static unsigned char y;
@@ -131,9 +62,6 @@ void __fastcall__ rvDecode(struct RiscV* cpu, const u32* raw) {
                 case 0x2: // slti/lw
                 case 0x3: { // sltiu
                     cpu->instr.imm = (raw->v >> 20) & 0xFFF;
-                   
-                    if (cpu->instr.imm & 0x800)
-                        cpu->instr.imm |= 0xFFFFF000;
                     break;
                 }
             }
@@ -146,9 +74,6 @@ void __fastcall__ rvDecode(struct RiscV* cpu, const u32* raw) {
                 case 0x2: { // sw
                     cpu->instr.rs2 = (raw->v >> 20) & 0x1f;
                     cpu->instr.imm = (((raw->v >> 25) & 0x7F) << 5) | ((raw->v >> 7) & 0x1f);
-                    
-                    if (cpu->instr.imm & 0x800) 
-                        cpu->instr.imm |= 0xFFFFF000;
                     break;
                 }
             }
@@ -314,19 +239,32 @@ void __fastcall__ rvExecute(struct RiscV* cpu) {
                     break;
                 }
                 case 0x1: { // lh
-                    // u32 addr;
-                    // addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm.v;
-                    // cpu->regs[cpu->instr.rd].v = (long int)(busLoad(&cpu->bus, addr.v, 16)[0] | (busLoad(&cpu->bus, addr.v, 16)[1] << 8));
-                    // break;
+                    PUT("lh");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    cpu->regs[cpu->instr.rd].v = (long)(signed int)(
+                        busLoad(&cpu->bus, addr.v, 16)->b[0] | 
+                        (busLoad(&cpu->bus, addr.v, 16)->b[1] << 8));
+                    break;
                 }
                 case 0x2: // lw
-                    // u32 addr;
-                    // addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm.v;
-                    // cpu->regs[cpu->instr.rd].v = (*busLoad(&cpu->bus, addr.v, 32)).v;
-                    // break;
+                    PUT("lw");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    cpu->regs[cpu->instr.rd].v = busLoad(&cpu->bus, addr.v, 32)->v;
+                    break;
                 case 0x4: // lbu
+                    PUT("lbu");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    cpu->regs[cpu->instr.rd].b[0] = busLoad(&cpu->bus, addr.v, 8)->b[0];
                     break;
                 case 0x5: // lhu
+                    PUT("lhu");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    cpu->regs[cpu->instr.rd].v = busLoad(&cpu->bus, addr.v, 16)->b[0] | (busLoad(&cpu->bus, addr.v, 16)->b[1] << 8);
+                    break;
                     break;
             }
 
@@ -342,6 +280,21 @@ void __fastcall__ rvExecute(struct RiscV* cpu) {
 
                     addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
                     busStore(&cpu->bus, addr.v, 8, &cpu->regs[cpu->instr.rs2]);
+                    break;
+                }
+                case 0x1: { // sh
+                    PUT("sh");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    busStore(&cpu->bus, addr.v, 16, &cpu->regs[cpu->instr.rs2]);
+                    break;
+                }
+                case 0x2: { // sw
+                    PUT("sw");
+
+                    addr.v = cpu->regs[cpu->instr.rs1].v + cpu->instr.imm;
+                    busStore(&cpu->bus, addr.v, 32, &cpu->regs[cpu->instr.rs2]);
+                    break;
                 }
             }
 
--- src/riscv.h
+++ src/riscv.h
@@ -4,6 +4,75 @@
 #include "types.h"
 #include "bus.h"
 
+#define X0  R_ZERO
+#define X1  R_RA
+#define X2  R_SP
+#define X3  R_GP
+#define X4  R_TP
+#define X5  R_T0
+#define X6  R_T1
+#define X7  R_T2
+#define X8  R_S0
+#define X9  R_S1
+#define X10 R_A0
+#define X11 R_A1
+#define X12 R_A2
+#define X13 R_A3
+#define X14 R_A4
+#define X15 R_A5
+#define X16 R_A6
+#define X17 R_A7
+#define X18 R_S2
+#define X19 R_S3
+#define X20 R_S4
+#define X21 R_S5
+#define X22 R_S6
+#define X23 R_S7
+#define X24 R_S8
+#define X25 R_S9
+#define X26 R_S10
+#define X27 R_S11
+#define X28 R_T3
+#define X29 R_T4
+#define X30 R_T5
+#define X31 R_T6
+
+enum Registers {
+    R_ZERO = 0,
+    R_RA = 1,
+    R_SP = 2,
+    R_GP = 3,
+    R_TP = 4,
+    R_T0 = 5,
+    R_T1 = 6,
+    R_T2 = 7,
+    R_S0 = 8,
+    R_FP = 8,
+    R_S1 = 9,
+    R_A0 = 10,
+    R_A1 = 11,
+    R_A2 = 12,
+    R_A3 = 13,
+    R_A4 = 14,
+    R_A5 = 15,
+    R_A6 = 16,
+    R_A7 = 17,
+    R_S2 = 18,
+    R_S3 = 19,
+    R_S4 = 20,
+    R_S5 = 21,
+    R_S6 = 22,
+    R_S7 = 23,
+    R_S8 = 24,
+    R_S9 = 25,
+    R_S10 = 26,
+    R_S11 = 27,
+    R_T3 = 28,
+    R_T4 = 29,
+    R_T5 = 30,
+    R_T6 = 31
+};
+
 struct RiscVInstr {
     unsigned char opcode;
     unsigned char rd;
