98fc8cec79ab3a855acd2aeaeca7726022a86978
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-18 22:22:20 +0300

    refactor(main): minor

--- src/main.c
+++ src/main.c
@@ -2,13 +2,27 @@
 #include "config.h"
 #include "riscv.h"
 #include "../libs/neslib.h"
-#include "machineCodes.h"
 
 #pragma bss-name(push, "ZEROPAGE")
 static u32* instr;
 unsigned char hasJump;
 #pragma bss-name(pop)
 static struct RiscV cpu;
+
+static const unsigned char code[] = {
+	// addi sp, sp, -8
+    0x13, 0x01, 0x81, 0xff,
+    // li	t1, -500
+    0x13, 0x03, 0xc0, 0xe0,
+	// sh   t1, 0(sp)
+    0x23, 0x10, 0x61, 0x00,
+	// lh   t2, 0(sp)
+    0x83, 0x13, 0x01, 0x00,
+    // sll 	t3, t2, 5
+    0x13, 0x9e, 0x53, 0x00,
+   	// addi sp, sp, 8
+    0x13, 0x01, 0x81,0x00,
+};
 
 void main(void) {
 	ppu_off(); // screen off
@@ -19,7 +33,7 @@ void main(void) {
 
 	hasJump = 0;
 
-	memcpy(&cpu.bus.dram.mem, beq, sizeof(beq));
+	memcpy(&cpu.bus.dram.mem, code, sizeof(code));
 	
 	while (1) { 
         instr = rvFetch(&cpu);
@@ -39,7 +53,6 @@ void main(void) {
 	rvDumpReg(&cpu);
 	
 	ppu_on_all(); // turn on screen1
-	
 	
 	while (1){}
 }
