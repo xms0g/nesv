558d3f5bacdc96cb216b4381f9584c5de4a3f53d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-28 15:49:14 +0300

    refactor(dram): minor

--- src/dram.c
+++ src/dram.c
@@ -15,14 +15,13 @@ unsigned long* __fastcall__ drmLoad(const struct DRAM* dram, const unsigned long
                 data = (unsigned long)dram->mem[offset];
                 break;
             case 16:
-                data = (unsigned long)dram->mem[offset] | 
-                        (unsigned long)dram->mem[offset + 1] << 8;
+                data = (unsigned long)dram->mem[offset++] | (unsigned long)dram->mem[offset] << 8;
                 break;
             case 32:
-                data = (unsigned long)dram->mem[offset] | 
-                        (unsigned long)dram->mem[offset + 1] << 8  |
-                        (unsigned long)dram->mem[offset + 2] << 16 |
-                        (unsigned long)dram->mem[offset + 3] << 24;
+                data = (unsigned long)dram->mem[offset++] | 
+                        (unsigned long)dram->mem[offset++] << 8 |
+                        (unsigned long)dram->mem[offset++] << 16 | 
+                        (unsigned long)dram->mem[offset] << 24;
                 break;
         }
         return &data;
@@ -39,14 +38,14 @@ void __fastcall__ drmStore(struct DRAM* dram, const unsigned long* address, unsi
                 dram->mem[offset] = (unsigned char)(*value & 0xFF);
                 break;
             case 16:
-                dram->mem[offset] = (unsigned char)(*value & 0xFF);
-                dram->mem[offset + 1] = (unsigned char)((*value >> 8) & 0xFF);
+                dram->mem[offset++] = (unsigned char)(*value & 0xFF);
+                dram->mem[offset] = (unsigned char)((*value >> 8) & 0xFF);
                 break;
             case 32:
-                dram->mem[offset] = (unsigned char)(*value & 0xFF);
-                dram->mem[offset + 1] = (unsigned char)((*value >> 8) & 0xFF);
-                dram->mem[offset + 2] = (unsigned char)((*value >> 16) & 0xFF);
-                dram->mem[offset + 3] = (unsigned char)((*value >> 24) & 0xFF);
+                dram->mem[offset++] = (unsigned char)(*value & 0xFF);
+                dram->mem[offset++] = (unsigned char)((*value >> 8) & 0xFF);
+                dram->mem[offset++] = (unsigned char)((*value >> 16) & 0xFF);
+                dram->mem[offset] = (unsigned char)((*value >> 24) & 0xFF);
                 break;
         }
     }
