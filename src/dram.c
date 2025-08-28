e9d06a52fdd23ebd76502b12448035d7ef0ec8bb
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-08-28 17:14:03 +0300

    perf(dram): removed long value, kept pointer instead of

--- src/dram.c
+++ src/dram.c
@@ -3,25 +3,25 @@
 
 #pragma bss-name(push, "ZEROPAGE")
 unsigned long data;
-unsigned long offset;
+unsigned char* offset;
 #pragma bss-name(pop)
 
 unsigned long* __fastcall__ drmLoad(const struct DRAM* dram, const unsigned long* address, unsigned char size) {
     if (*address >= DRAM_BASE && *address < DRAM_BASE + DRAM_SIZE) {
-        offset = *address - DRAM_BASE;
+        offset = (unsigned char*)&dram->mem[*address - DRAM_BASE];
 
         switch (size) {
             case 8:
-                data = (unsigned long)dram->mem[offset];
+                data = *offset;
                 break;
             case 16:
-                data = (unsigned long)dram->mem[offset++] | (unsigned long)dram->mem[offset] << 8;
+                data = (unsigned long)*offset++ | (unsigned long)*offset << 8;
                 break;
             case 32:
-                data = (unsigned long)dram->mem[offset++] | 
-                        (unsigned long)dram->mem[offset++] << 8 |
-                        (unsigned long)dram->mem[offset++] << 16 | 
-                        (unsigned long)dram->mem[offset] << 24;
+                data = (unsigned long)*offset++ | 
+                        (unsigned long)*offset++ << 8 |
+                        (unsigned long)*offset++ << 16 | 
+                        (unsigned long)*offset << 24;
                 break;
         }
         return &data;
@@ -31,21 +31,21 @@ unsigned long* __fastcall__ drmLoad(const struct DRAM* dram, const unsigned long
 
 void __fastcall__ drmStore(struct DRAM* dram, const unsigned long* address, unsigned char size, const unsigned long* value) {
     if (*address >= DRAM_BASE && *address < DRAM_BASE + DRAM_SIZE) {
-        offset = *address - DRAM_BASE;
+        offset = &dram->mem[*address - DRAM_BASE];
 
         switch (size) {
             case 8:
-                dram->mem[offset] = (unsigned char)(*value & 0xFF);
+                *offset = (unsigned char)(*value & 0xFF);
                 break;
             case 16:
-                dram->mem[offset++] = (unsigned char)(*value & 0xFF);
-                dram->mem[offset] = (unsigned char)((*value >> 8) & 0xFF);
+                *offset++ = (unsigned char)(*value & 0xFF);
+                *offset = (unsigned char)((*value >> 8) & 0xFF);
                 break;
             case 32:
-                dram->mem[offset++] = (unsigned char)(*value & 0xFF);
-                dram->mem[offset++] = (unsigned char)((*value >> 8) & 0xFF);
-                dram->mem[offset++] = (unsigned char)((*value >> 16) & 0xFF);
-                dram->mem[offset] = (unsigned char)((*value >> 24) & 0xFF);
+                *offset++ = (unsigned char)(*value & 0xFF);
+                *offset++ = (unsigned char)((*value >> 8) & 0xFF);
+                *offset++ = (unsigned char)((*value >> 16) & 0xFF);
+                *offset = (unsigned char)((*value >> 24) & 0xFF);
                 break;
         }
     }
